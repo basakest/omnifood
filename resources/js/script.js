e8ce3b0039d8dc66d9efcaed6c2074408dfdc5da
basakest <ab1652759879@gmail.com>
2020-02-09 15:52:11 +0800

    add js of sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,13 @@
 /*jslint browser: true*/
 /*global $, document*/
 $(document).ready(function() {
-    
+   $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
 });
